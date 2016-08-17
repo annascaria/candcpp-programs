e3830f3a2fe78812ad7d19c3835bd47ff2203bf1
annascaria <bini.annscaria879@gmail.com>
2016-08-17 17:26:17 +0530

    Add files via upload

---
+++ check_whether_odd_or_even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int i;
+printf("Enter the number\n");
+scanf("%d",&i);
+if(i%2==0)
+{
+printf("The number entered is even\n");
+}
+else
+{
+printf("The number entered is odd\n");
+}
+}
